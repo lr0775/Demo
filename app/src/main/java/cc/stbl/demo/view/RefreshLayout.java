b8f8218039d3165638ea748180c2b7b8ca144a2d
lingrui <lingrui@stbl.cc>
2016-12-07 10:57:57 +0800

    提交代码

--- app/src/main/java/cc/stbl/demo/view/RefreshLayout.java
+++ app/src/main/java/cc/stbl/demo/view/RefreshLayout.java
@@ -2,8 +2,11 @@ package cc.stbl.demo.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+
+import cc.stbl.demo.R;
 
 /**
  * Created by Administrator on 2016/12/6.
@@ -13,6 +16,17 @@ public class RefreshLayout extends ViewGroup {
 
     private int mTouchSlop;
 
+    private View mHeaderView;
+    private View mTargetView;
+    private View mFooterView;
+
+    private int mLayoutHeight;
+    private int mHeaderWidth;
+    private int mHeaderHeight;
+    private int mTargetWidth;
+    private int mTargetHeight;
+    private int mFooterWidth;
+    private int mFooterHeight;
 
     public RefreshLayout(Context context) {
         this(context, null);
@@ -30,7 +44,49 @@ public class RefreshLayout extends ViewGroup {
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        int childCount = getChildCount();
+        if (childCount == 0) {
+            return;
+        }
+        mHeaderView = findViewById(R.id.swipe_refresh_header);
+        mTargetView = findViewById(R.id.swipe_target);
+        mFooterView = findViewById(R.id.swipe_load_more_footer);
+        if (mTargetView == null) {
+            return;
+        }
+        if (mHeaderView != null) {
+            mHeaderView.setVisibility(GONE);
+        }
+        if (mFooterView != null) {
+            mFooterView.setVisibility(GONE);
+        }
+    }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mLayoutHeight = getMeasuredHeight();
+        mHeaderWidth = mHeaderView.getMeasuredWidth();
+        mHeaderHeight = mHeaderView.getMeasuredHeight();
+        mTargetWidth = mTargetView.getMeasuredWidth();
+        mTargetHeight = mTargetView.getMeasuredHeight();
+        mFooterWidth = mFooterView.getMeasuredWidth();
+        mFooterHeight = mFooterView.getMeasuredHeight();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (mTargetView == null) {
+            return;
+        }
+        if (mHeaderView != null) {
+            mHeaderView.layout(0, -mHeaderHeight, mHeaderWidth, 0);
+        }
+        mTargetView.layout(0, 0, mTargetWidth, mTargetHeight);
+        if (mFooterView != null) {
+            mFooterView.layout(0, mLayoutHeight, mFooterWidth, mLayoutHeight + mFooterHeight);
+        }
     }
 }
